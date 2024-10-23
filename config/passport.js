5bcf8ebc81a55d31bc8c7447b472d6ae27fcc841
Bagadi Bharat <bharatbagadi923@gmail.com>
2024-10-23 21:24:12 +0530

    added

--- config/passport.js
+++ config/passport.js
@@ -1,148 +1,220 @@
 var passport = require('passport');
 var User = require('../models/user');
-const MailSender = require('../mail');
-var LocalStrategy = require('passport-local').Strategy;
-const { validationResult } = require('express-validator'); // Assuming you're using express-validator
+const MailSender = require('../mail')
 
-// Serialize and deserialize user for session management
+var LocalStrategy = require('passport-local').Strategy;
+
+
 passport.serializeUser(function (user, done) {
     done(null, user.id);
+
 });
 
 passport.deserializeUser(function (id, done) {
     User.findById(id, function (err, user) {
         done(err, user);
     });
+
 });
 
-// Email validation
 function validateEmail(email) {
     const re = /^[a-zA-Z0-9._%+-]+@gvpce\.ac\.in$/; // This only allows GVPCE domain emails
     return re.test(email);
 }
 
-// Password validation
 function validatePassword(password) {
-    const errors = [];
-    if (password.length < 6) {
+    var p = password
+    errors = [];
+    if (p.length < 6) {
         errors.push("Your password must be at least 6 characters.");
     }
-    if (password.length > 15) {
-        errors.push("Your password must be at most 15 characters.");
+    if (p.length > 15) {
+        errors.push("Your password must be atmost 15 characters.");
     }
-    if (!/[A-Z]/.test(password)) {
-        errors.push("Your password must contain at least one uppercase letter.");
+    if (p.search(/[A-Z]/i) < 0) {
+        errors.push("Your password must contain at least one upercase letter.");
     }
-    if (!/[a-z]/.test(password)) {
+    if (p.search(/[a-z]/i) < 0) {
         errors.push("Your password must contain at least one lowercase letter.");
     }
-    if (!/[0-9]/.test(password)) {
+    if (p.search(/[0-9]/) < 0) {
         errors.push("Your password must contain at least one digit.");
     }
-    if (!/[!@#$%^&*]/.test(password)) {
+    if (p.search(/[!@#$%^&*]/) < 0) {
         errors.push("Your password must contain at least one special character.");
     }
     return errors;
 }
 
-// Local strategy for user registration
 passport.use('local-register', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
 },
-    async function (req, email, password, done) {
-        const { name, classs, rollnumber, teacher, student, year } = req.body;
-        let messages = [];
-
-        // Input validation
+    function (req, email, password, done) {
+        var name = req.body.name;
+        var classs = req.body.class
+        var rollnumber = req.body.rollnumber
+        var teacher = req.body.teacher
+        var student = req.body.student
+        var messages = [];
+        var year = req.body.year
         req.checkBody('email', 'Invalid email').notEmpty().isEmail();
         req.checkBody('password', 'Invalid password').notEmpty();
-
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            errors.array().forEach(error => messages.push(error.msg));
-            return done(null, false, req.flash('error', messages));
-        }
-        
-        if (!validateEmail(email)) {
-            messages.push("Email Domain: @gvpce.ac.in required");
-            return done(null, false, req.flash('error', messages));
-        }
-
-        const passwordErrors = validatePassword(password);
-        if (passwordErrors.length > 0) {
-            return done(null, false, req.flash('error', passwordErrors));
-        }
-
-        if (!teacher && !student) {
-            messages.push("Please check the tickbox");
-            return done(null, false, req.flash('error', messages));
-        }
-
-        if (year === "Year") {
-            messages.push("Please select a year");
-            return done(null, false, req.flash('error', messages));
-        }
-
-        if (classs === "Class") {
-            messages.push("Please select a class");
-            return done(null, false, req.flash('error', messages));
-        }
-
-        try {
-            // Check for rollnumber if provided
-            if (rollnumber) {
-                const existingUser = await User.findOne({ rollnumber });
-                if (existingUser) {
-                    return done(null, false, { message: 'Roll number already in use.' });
-                }
-            }
-
-            // Check if email already exists
-            const existingEmail = await User.findOne({ email });
-            if (existingEmail) {
-                return done(null, false, { message: 'Email already in use.' });
-            }
-
-            // Create and save the new user
-            const newUser = new User({
-                name,
-                email,
-                password: User.encryptPassword(password),
-                class: classs,
-                rollnumber: rollnumber || null,
-                who: teacher ? "teacher" : "student",
-                year: student ? year : null
+        var errors = req.validationErrors();
+        if (errors) {
+            errors.forEach(function (error) {
+                messages.push(error.msg);
             });
-
-            await newUser.save();
-            return done(null, newUser);
-
-        } catch (err) {
-            return done(err);
+            return done(null, false, req.flash('error', messages));
         }
-    }
-));
+        else if (!validateEmail(email)) {
+            messages.push("Email Domain: @gvpce.ac.in required")
+            return done(null, false, req.flash('error', messages));
+        }
+        else if ((validatePassword(password)).length != 0) {
+            messages = validatePassword(password)
+            return done(null, false, req.flash('error', messages));
+        }
+        else if (!teacher && !student) {
+            messages.push("Please check the tickbox")
+            return done(null, false, req.flash('error', messages));
+        }
+        else if (year == "Year") {
+            messages.push("Please select a year")
+            return done(null, false, req.flash('error', messages));
+        }
+        else if (classs == "Class") {
+            messages.push("Please select a class")
+            return done(null, false, req.flash('error', messages));
+        }
+        else {
+            if (rollnumber) {
+                User.findOne({ 'rollnumber': rollnumber }, function (err, user) {
+                    if (err) {
+                        return done(err)
+                    }
+                    if (user) {
+                        console.log(rollnumber)
+                        return done(null, false, { message: 'Roll number already in use.' });
+                    }
+                    else {
+                        User.findOne({ 'email': email }, function (err, user) {
+                            if (err) {
+                                return done(err);
+                            }
+                            if (user) {
+                                return done(null, false, { message: 'Email already in use.' });
+                            }
+                            else {
+                                var newUser = new User();
+                                newUser.name = name;
+                                newUser.email = email;
+                                newUser.password = newUser.encryptPassword(password);
+                                newUser.class = classs
+                                newUser.rollnumber = rollnumber
+                                if (teacher == "0") {
+                                    newUser.who = teacher
+                                }
+                                else if (student) {
+                                    newUser.who = student
+                                    if (!year) {
+                                        messages.push("Please enter all details")
+                                        return done(null, false, req.flash('error', messages));
+                                    }
+                                    else {
+                                        newUser.year = year
+                                    }
+                                }
+                                else {
+                                    newUser.who = ""
+                                }
+                                newUser.save(function (err, result) {
+                                    if (err) {
+                                        throw err;
+                                    }
+                                    return done(null, newUser);
+                                });
+                            }
 
-// Local strategy for user login
+
+
+                        });
+                    }
+                })
+            } else {
+                User.findOne({ 'email': email }, function (err, user) {
+                    if (err) {
+                        return done(err);
+                    }
+                    if (user) {
+                        return done(null, false, { message: 'Email already in use.' });
+                    }
+                    else {
+                        var newUser = new User();
+                        newUser.name = name;
+                        newUser.email = email;
+                        newUser.password = newUser.encryptPassword(password);
+                        newUser.class = classs
+                        newUser.rollnumber = rollnumber
+                        if (teacher == "0") {
+                            newUser.who = teacher
+                        }
+                        else if (student) {
+                            newUser.who = student
+                            if (!year) {
+                                messages.push("Please enter all details")
+                                return done(null, false, req.flash('error', messages));
+                            }
+                            else {
+                                newUser.year = year
+                            }
+                        }
+                        else {
+                            newUser.who = ""
+                        }
+                        newUser.save(function (err, result) {
+                            if (err) {
+                                throw err;
+                            }
+                            return done(null, newUser);
+                        });
+                    }
+
+
+
+                });
+            }
+
+        }
+
+    }));
+
+
 passport.use('local-login', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
 },
-    async function (req, email, password, done) {
+    function (req, email, password, done) {
+
+        //Validation Errors
         req.checkBody('email', 'Invalid Email').notEmpty().isEmail();
         req.checkBody('password', 'Invalid password').notEmpty();
-
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            const messages = errors.array().map(err => err.msg);
+        var errors = req.validationErrors();
+        if (errors) {
+            var messages = [];
+            errors.forEach(function (error) {
+                messages.push(error.msg);
+            });
             return done(null, false, req.flash('error', messages));
         }
 
-        try {
-            const user = await User.findOne({ email });
+        //Find user by email
+        User.findOne({ 'email': email }, function (err, user) {
+            if (err) {
+                return done(err);
+            }
             if (!user) {
                 return done(null, false, { message: 'User not found.' });
             }
@@ -153,8 +225,7 @@ passport.use('local-login', new LocalStrategy({
 
             return done(null, user);
 
-        } catch (err) {
-            return done(err);
-        }
-    }
-));
+        });
+
+    }));
+
